0b7105b21d533d1269b094a44f79bec00ed5d504
ChapasJ <j.treus@proton.me>
2026-08-09 19:11:40 +0200

    Programación Confirmada

---
+++ src/data/programacionOSC.js
@@ -0,0 +1,261 @@
+// src/data/programacionOSC.js
+
+export const programacionOSC = [
+  // ─────────────────────────────────
+  // VIERNES
+  // ─────────────────────────────────
+
+  {
+    id: "espectaculo-01",
+    fecha: "2026-09-18",
+    inicio: "21:00",
+    fin: "22:45",
+    tipo: "espectaculo",
+    titulo: "Ciencia en escena",
+    entidad: "Comisión Espectaculos",
+    lugar: "Anfiteatro",
+    descripcion:
+      "Un espectáculo donde los experimentos y la divulgación científica se encuentran sobre el escenario."
+  },
+
+  // ─────────────────────────────────
+  // SÁBADO
+  // ─────────────────────────────────
+
+  {
+    id: "charla-01",
+    fecha: "2026-09-19",
+    inicio: "12:00",
+    fin: "12:15",
+    tipo: "charla",
+    titulo: "Acto de apertura",
+    entidad: "OSC",
+    lugar: "Anfiteatro",
+    descripcion:
+      "Los codirectores de está edición Juan y Jairo darán por inaugurada la OSC2026"
+  },
+
+  {
+    id: "espectaculo-02",
+    fecha: "2026-09-19",
+    inicio: "12:15",
+    fin: "12:30",
+    tipo: "espectaculo",
+    titulo: "Flashmob",
+    entidad: "Comisión Espectaculos",
+    lugar: "Anfiteatro",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "charla-02",
+    fecha: "2026-09-19",
+    inicio: "12:30",
+    fin: "13:00",
+    tipo: "charla",
+    titulo: "Alzheimer en ratones",
+    entidad: "Mariña Rodriguez",
+    lugar: "Colegio",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "taller-01",
+    fecha: "2026-09-19",
+    inicio: "12:45",
+    fin: "13:15",
+    tipo: "taller",
+    titulo: "Quiero ser paleontólog@",
+    entidad: "Espiciencia",
+    lugar: "Anfiteatro",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "charla-03",
+    fecha: "2026-09-19",
+    inicio: "13:00",
+    fin: "13:30",
+    tipo: "charla",
+    titulo: "",
+    entidad: "Mónica Castro",
+    lugar: "Colegio",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "charla-04",
+    fecha: "2026-09-19",
+    inicio: "13:30",
+    fin: "14:00",
+    tipo: "charla",
+    titulo: "",
+    entidad: "Manuel Debasa",
+    lugar: "Colegio",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "taller-02",
+    fecha: "2026-09-19",
+    inicio: "16:00",
+    fin: "17:00",
+    tipo: "taller",
+    titulo: "El teléfono móvil: un laboratorio de bolsillo",
+    entidad: "Luis Carlos",
+    lugar: "Colegio",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "taller-03",
+    fecha: "2026-09-19",
+    inicio: "16:00",
+    fin: "16:30",
+    tipo: "taller",
+    titulo: "Cianotipiaa y pigmentos naturales",
+    entidad: "Espiciencia",
+    lugar: "Anfiteatro",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "charla-05",
+    fecha: "2026-09-19",
+    inicio: "16:30",
+    fin: "17:00",
+    tipo: "charla",
+    titulo: "Seguridad en el hogar",
+    entidad: "Protección Civil",
+    lugar: "Anfiteatro",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "taller-04",
+    fecha: "2026-09-19",
+    inicio: "17:00",
+    fin: "17:45",
+    tipo: "taller",
+    titulo: "Curiosidades Matemáticas",
+    entidad: "CP San José-Josefinas",
+    lugar: "Colegio",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "espectaculo-03",
+    fecha: "2026-09-19",
+    inicio: "17:30",
+    fin: "18:30",
+    tipo: "espectaculo",
+    titulo: "Cienceneta",
+    entidad: "David Ballesteros",
+    lugar: "Anfiteatro",
+    descripcion:
+      "Una furgoneta cargada de Ciencia y humor nos visita, no os la podeis perder."
+  },
+
+  {
+    id: "espectaculo-04",
+    fecha: "2026-09-19",
+    inicio: "18:30",
+    fin: "18:45",
+    tipo: "espectaculo",
+    titulo: "Flashmob",
+    entidad: "Comisión Espectaculos",
+    lugar: "Anfiteatro",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "charla-06",
+    fecha: "2026-09-19",
+    inicio: "18:45",
+    fin: "19:00",
+    tipo: "charla",
+    titulo: "Despedida",
+    entidad: "OSC",
+    lugar: "Anfiteatro",
+    descripcion:
+      ""
+  },
+
+  // ─────────────────────────────────
+  // DOMINGO
+  // ─────────────────────────────────
+
+  {
+    id: "charla-07",
+    fecha: "2026-09-20",
+    inicio: "11:30",
+    fin: "12:00",
+    tipo: "charla",
+    titulo: "La Revolución Creativa",
+    entidad: "Alba Meijide",
+    lugar: "Colegio",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "taller-05",
+    fecha: "2026-09-20",
+    inicio: "10:30",
+    fin: "11:30",
+    tipo: "taller",
+    titulo: "El teléfono móvil: un laboratorio de bolsillo",
+    entidad: "Luis Carlos",
+    lugar: "Colegio",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "taller-06",
+    fecha: "2026-09-20",
+    inicio: "11:00",
+    fin: "11:30",
+    tipo: "taller",
+    titulo: "Quiero ser paleontólog@",
+    entidad: "Espiciencia",
+    lugar: "Anfiteatro",
+    descripcion:
+      ""
+  },
+
+  {
+    id: "espectaculo-05",
+    fecha: "2026-09-20",
+    inicio: "12:45",
+    fin: "13:00",
+    tipo: "espectaculo",
+    titulo: "Flashmob",
+    entidad: "Comisión Espectaculos",
+    lugar: "Anfiteatro",
+    descripcion:
+      ""
+  },
+  {
+    id: "charla-08",
+    fecha: "2026-09-20",
+    inicio: "13:00",
+    fin: "14:00",
+    tipo: "charla",
+    titulo: "Acto de clausura",
+    entidad: "OSC",
+    lugar: "Anfiteatro",
+    descripcion:
+      ""
+  },
+];
