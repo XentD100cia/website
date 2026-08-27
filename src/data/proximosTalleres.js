f71d11e0d1436477643f5b264d3c6d336a10cb39
ChapasJ <j.treus@proton.me>
2026-08-27 11:02:22 +0200

    Taller antes da OSC

--- src/data/proximosTalleres.js
+++ src/data/proximosTalleres.js
@@ -1,3 +1,16 @@
 export const proximosTalleres = [
-  
+  {
+    title: 'Imos so espazo?',
+    estado: 'programado',
+    date: '03 de setembro',
+    sortDate: '2026-09-03',
+    edad: 'Todos os publicos',
+    lugar: 'Cambre',
+    excerpt:
+      'Gustaríate crear o teu propio foguete de auga? Apúntate e prepárate para a Open Science Cambre. Tras rematar e perfeccionar o teu foguete de auga poderas sumerxirte nunha competición, que terá lugar na OSC26, o día 20 de setembro.',
+    button: 'Me interesa',
+    href: 'https://forms.gle/mEQH3q2vS7bNxFqx9',
+    image: '/images/images/talleres/Imos.ao.espazo.png'
+  },
+
 ];
